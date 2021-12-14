f59f17e2a3804cc35a7f7a473620e65ddefb3ec0
eom4961 <kia6013@gmail.com>
2021-12-15 01:41:19 +0900

    Message for log

--- app/src/main/java/com/example/qrcode/CreateQR.java
+++ app/src/main/java/com/example/qrcode/CreateQR.java
@@ -1,23 +1,18 @@
 package com.example.qrcode;
 
+
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.content.Intent;
 import android.graphics.Bitmap;
 import android.os.Bundle;
-import android.service.autofill.UserData;
 import android.widget.ImageView;
 
-import com.google.firebase.database.DataSnapshot;
-import com.google.firebase.database.DatabaseReference;
-import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 import com.google.zxing.BarcodeFormat;
 import com.google.zxing.MultiFormatWriter;
 import com.google.zxing.common.BitMatrix;
 import com.journeyapps.barcodescanner.BarcodeEncoder;
-
-import java.util.HashMap;
-import java.util.Map;
 
 public class CreateQR extends AppCompatActivity {
     private ImageView iv;
@@ -35,16 +30,16 @@ public class CreateQR extends AppCompatActivity {
 
         iv = findViewById(R.id.qrcode);
         //QR 스켄시 이동
-        text = "conditionRef";
+        FirebaseUser user = FirebaseAuth.getInstance().getCurrentUser();
+
+        text = user.getUid();
         MultiFormatWriter multiFormatWriter = new MultiFormatWriter();
-        try{
-            BitMatrix bitMatrix = multiFormatWriter.encode(text, BarcodeFormat.QR_CODE,200,200);
+        try {
+            BitMatrix bitMatrix = multiFormatWriter.encode(text, BarcodeFormat.QR_CODE, 200, 200);
             BarcodeEncoder barcodeEncoder = new BarcodeEncoder();
             Bitmap bitmap = barcodeEncoder.createBitmap(bitMatrix);
             iv.setImageBitmap(bitmap);
-        }catch (Exception e){}
+        } catch (Exception e) {
+        }
     }
-
-
-
 }
--- app/src/main/java/com/example/qrcode/ScanQR.java
+++ app/src/main/java/com/example/qrcode/ScanQR.java
@@ -1,30 +1,37 @@
 package com.example.qrcode;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
+import android.widget.EditText;
 import android.widget.TextView;
 import android.widget.Toast;
 
-import com.google.firebase.database.DataSnapshot;
-import com.google.firebase.database.DatabaseError;
-import com.google.firebase.database.DatabaseReference;
-import com.google.firebase.database.FirebaseDatabase;
-import com.google.firebase.database.ValueEventListener;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.firestore.DocumentReference;
+import com.google.firebase.firestore.DocumentSnapshot;
+import com.google.firebase.firestore.FirebaseFirestore;
 import com.google.zxing.integration.android.IntentIntegrator;
 import com.google.zxing.integration.android.IntentResult;
 
+import java.util.HashMap;
+import java.util.Map;
 
-git
 
 public class ScanQR extends AppCompatActivity {
-
+    private static final String TAG = "Main_Activity";
+    private static FirebaseFirestore db = FirebaseFirestore.getInstance();
+    TextView addressTxet, birthDayTxet, feverTxat, heightTxet, medicineTxet, nameTxet, phoneNumberTxet, weightTxet;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_scan_qr);
         new IntentIntegrator(this).initiateScan();
+
 
     }
 
@@ -38,15 +45,36 @@ public class ScanQR extends AppCompatActivity {
             } else {
                 Toast.makeText(this, "Scanned: " + result.getContents(), Toast.LENGTH_LONG).show();
                 // todo
+                DocumentReference docRef = db.collection("users").document(result.getContents());
+                docRef.get().addOnCompleteListener(new OnCompleteListener<DocumentSnapshot>() {
+                    @Override
+                    public void onComplete(@NonNull Task<DocumentSnapshot> task) {
+                        if (task.isSuccessful()) {
+                            DocumentSnapshot document = task.getResult();
+                            if (document.exists()) {
+                                Log.d(TAG, "DocumentSnapshot data: " + document.getData().toString());
+                                Map<String, Object> getData = document.getData();
+                                addressTxet.setText(getData.get("address").toString());
+                                birthDayTxet.setText(getData.get("birthDay").toString());
+                                feverTxat.setText(getData.get("fever").toString());
+                                heightTxet.setText(getData.get("height").toString());
+                                medicineTxet.setText(getData.get("medicine").toString());
+                                nameTxet.setText(getData.get("name").toString());
+                                phoneNumberTxet.setText(getData.get("phoneNumber").toString());
+                                weightTxet.setText(getData.get("weight").toString());
+
+
+                            } else {
+                                Log.d(TAG, "No such document");
+                            }
+                        } else {
+                            Log.d(TAG, "get failed with ", task.getException());
+                        }
+                    }
+                });
             }
         } else {
             super.onActivityResult(requestCode, resultCode, data);
-
-
-
-
-
-
         }
     }
 
